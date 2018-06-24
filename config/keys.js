d2f6f1741187b65456b61fee8d63ad1fbe186d98
Nicholas Halloran <nmhalloran@gmail.com>
2018-06-23 20:47:11 -0700

    complete user auth

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://nick:go_team4_go@ds117061.mlab.com:17061/parksniffdb"
+  mongoURI: "mongodb://nick:go_team4_go@ds117061.mlab.com:17061/parksniffdb",
+  secretOrKey: "secret"
 };
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 const app = express();
+
+// Body parser middleware
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB config
 
@@ -19,6 +26,11 @@ mongoose
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello"));
+// Passport Middleware
+app.use(passport.initialize());
+
+// Passport Config
+require("./config/passport")(passport);
 
 // Use Routes
 app.use("/api/users", users);
