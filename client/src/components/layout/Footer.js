773ecf68ef4ce3c371f54f94fc6a3523d254c9aa
Nicholas Halloran <nmhalloran@gmail.com>
2018-06-24 23:18:08 -0700

    stubbed out landing page

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { CLIENT_RENEG_LIMIT } from "tls";
+
+export default () => {
+  return (
+    <footer className="bg-dark text-white mt-5 p-4 text-center">
+      Copyright &copy; {new Date().getFullYear()} Parking Sniffer
+    </footer>
+  );
+};
