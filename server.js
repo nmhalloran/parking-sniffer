1c40f992e8d4942e6453d91ce83636f9fb6de940
Nicholas Halloran <nmhalloran@gmail.com>
2018-06-26 18:00:46 -0700

    updated User.js file

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const bodyParser = require("body-parser");
 const passport = require("passport");
 
 const users = require("./routes/api/users");
-const profile = require("./routes/api/profile");
-const posts = require("./routes/api/posts");
 
 const app = express();
 
@@ -25,19 +23,17 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.sendFile(__dirname + '/client/public/index.html'));
+app.get("/", (req, res) =>
+  res.sendFile(__dirname + "/client/public/index.html")
+);
 // Passport Middleware
 app.use(passport.initialize());
 
 // Passport Config
 require("./config/passport")(passport);
 
-
-
 // Use Routes
 app.use("/api/users", users);
-app.use("/api/profile", profile);
-app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
