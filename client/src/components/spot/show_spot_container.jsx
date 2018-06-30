50abdaa436712a8f1831032771f7973af6aa6d91
Meenakshi-Anand <abhi@codeanand.com>
2018-06-29 22:48:06 -0700

    create show vehicle container

--- client/src/components/spot/show_spot_container.jsx
+++ client/src/components/spot/show_spot_container.jsx
@@ -1,37 +1,23 @@
 import { connect } from "react-redux";
 import React from "react";
 
-import { fetchSpot } from "../../actions/spot_actions";
-import ShowSpot from "./show_spot";
+import { fetchVehicle,deleteVehicle } from "../../actions/vehicle_actions";
+import ShowVehicle from "./show_vehicle";
 
 const mapStateToProps = (state, ownProps) => {
   return {
-    // spot: state.entities.spots[ownProps.match.params.id], // spot from backend
+    // vehicle from backend
+    vehicle: state.entities.vehicles[ownProps.match.params.id],
     user: state.isAuthenticated.user,
-    spot: { // test spot
-      line1: '825',
-      line2: 'Battery St.',
-      city: 'San Francisco',
-      state: 'CA',
-      zipcode: '94111',
-      description: 'parking spots in a/A classrooms. Lorem ipsum dolor sit amet, error dolores inciderint eu ius, ex wisi vituperata mel. Ut veri everti legendos quo, an audiam epicuri laboramus qui, summo dissentiet ne his. Eum mazim veniam epicurei id. Vis dicit instructior in, vel omnes partem dolorem eu. Duo postea vulputate cu.',
-      vehicle_types: ['truck', 'fullsize'],
-      spot_type: 'open parking',
-      rental_rate: 1000,
-      rental_type: 'daily',
-      img_url: 'truck@truck.com',
-      reservations: [],
-      latitude: 37.798965,
-      longitude: -122.4013603
-    }
   };
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-  fetchSpot: id => dispatch(fetchSpot(id))
+  fetchVehicle: id => dispatch(fetchVehicle(id)),
+  deleteVehicle: id => dispatch(deleteVehicle(id))
 });
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(ShowSpot);
+)(ShowVehicle);
