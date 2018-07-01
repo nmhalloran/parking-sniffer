88acf426c0856ce3564a601e80f452480d38ea7c
Meenakshi-Anand <abhi@codeanand.com>
2018-06-30 19:03:47 -0700

    removed debuggers

--- client/src/components/vehicles/show_vehicle.jsx
+++ client/src/components/vehicles/show_vehicle.jsx
@@ -40,11 +40,11 @@ class ShowVehicle extends React.Component{
     return (
       <div>
         <h1>Vehicle Show Page</h1>
-        <h2>Vehicle Type: ${this.props.vehicle.vehicle_type}</h2>
-        <h2>Plate No: ${this.props.vehicle.plate_no}</h2>
-        <h2>Color: ${this.props.vehicle.color}</h2>
-        <h2>Model: ${this.props.vehicle.model}</h2>
-        <h2>Year: ${this.props.vehicle.year}</h2>
+        <h2>Vehicle Type: {this.props.vehicle.vehicle_type}</h2>
+        <h2>Plate No: {this.props.vehicle.plate_no}</h2>
+        <h2>Color: {this.props.vehicle.color}</h2>
+        <h2>Model: {this.props.vehicle.model}</h2>
+        <h2>Year: {this.props.vehicle.year}</h2>
         <button value="Delete Vehicle" onClick={ (e) => this.handleDelete(e) }>
           Delete Vehicle
         </button>
