4faa1dc172a053a36698e2aa35b35f575b0389ab
Nicholas Halloran <nmhalloran@gmail.com>
2018-06-30 18:16:28 -0700

    fix merge errors

--- client/src/actions/spot_actions.js
+++ client/src/actions/spot_actions.js
@@ -1,82 +1,66 @@
-import * as SpotAPIUtil from '../util/spot_api_util';
-import { CLEAR_ERRORS, ADD_ERRORS } from './errors_actions'
-export const RECEIVE_SPOTS = 'RECEIVE_SPOTS';
-export const RECEIVE_SPOT = 'RECEIVE_SPOT';
-export const REMOVE_SPOT = 'REMOVE_SPOT';
-export const RECEIVE_SPOT_ERRORS = 'RECEIVE_SPOT_ERRORS';
+import * as SpotAPIUtil from "../util/spot_api_util";
+import { CLEAR_ERRORS, ADD_ERRORS } from "./errors_actions";
+export const RECEIVE_SPOTS = "RECEIVE_SPOTS";
+export const RECEIVE_SPOT = "RECEIVE_SPOT";
+export const REMOVE_SPOT = "REMOVE_SPOT";
+export const RECEIVE_SPOT_ERRORS = "RECEIVE_SPOT_ERRORS";
 
 const receiveSpots = spots => ({
-    type: RECEIVE_SPOTS,
-    spots
-})
+  type: RECEIVE_SPOTS,
+  spots
+});
 
 const receiveSpot = spot => ({
-    type: RECEIVE_SPOT,
-    spot
-})
+  type: RECEIVE_SPOT,
+  spot
+});
 
-const removeSpot = (id) => ({
-    type: REMOVE_SPOT,
-    id
-})
+const removeSpot = id => ({
+  type: REMOVE_SPOT,
+  id
+});
 
 const receiveErrors = errors => ({
-    type: RECEIVE_SPOT_ERRORS,
-    errors
-})
+  type: RECEIVE_SPOT_ERRORS,
+  errors
+});
 
-export const fetchSpotsByGPS = (gps) => dispatch => (
-    SpotAPIUtil.fetchSpotsByGPS(gps)
-        .then(spots => dispatch(receiveSpots(spots)))
-        .catch(err => console.log(err)) //deal with errors later
-)
+export const fetchSpotsByGPS = gps => dispatch =>
+  SpotAPIUtil.fetchSpotsByGPS(gps)
+    .then(spots => dispatch(receiveSpots(spots)))
+    .catch(err => console.log(err)); //deal with errors later
 
-export const fetchSpotsByZip = (zip) => dispatch => (
-    SpotAPIUtil.fetchSpotsByZip(zip)
-        .then(spots => dispatch(receiveSpots(spots)))
-        .catch(err => console.log(err)) //deal with errors later
-)
+export const fetchSpotsByZip = zip => dispatch =>
+  SpotAPIUtil.fetchSpotsByZip(zip)
+    .then(spots => dispatch(receiveSpots(spots)))
+    .catch(err => console.log(err)); //deal with errors later
 
-<<<<<<< HEAD
-export const fetchSpotById = (id) => dispatch => (
-    SpotAPIUtil.fetchSpotById(id)
-        .then(spot => dispatch(receiveSpot(spot)))
-        .catch(error => dispatch({type: ADD_ERRORS, payload: error.response.data}))
-=======
-export const fetchSpotsByOwner = () => dispatch => (
-    SpotAPIUtil.fetchSpotsByOwner()
-        .then(spots => dispatch(receiveSpots(spots)))
-        .catch(err => console.log(err))
->>>>>>> alfred
-)
+export const fetchSpotById = id => dispatch =>
+  SpotAPIUtil.fetchSpotById(id)
+    .then(spot => dispatch(receiveSpot(spot)))
+    .catch(error =>
+      dispatch({ type: ADD_ERRORS, payload: error.response.data })
+    );
 
-export const fetchSpot = (id) => dispatch => (
-    SpotAPIUtil.fetchSpot(id)
-        .then(spot => dispatch(receiveSpot(spot)))
-        .catch(err => console.log(err))
-)
+export const fetchSpot = id => dispatch =>
+  SpotAPIUtil.fetchSpot(id)
+    .then(spot => dispatch(receiveSpot(spot)))
+    .catch(err => console.log(err));
 
-export const createSpot = (spot) => dispatch => {
+export const createSpot = spot => dispatch => {
+  // debugger
 
-    // debugger
+  return SpotAPIUtil.createSpot(spot)
+    .then(spotRes => dispatch(receiveSpot(spotRes)))
+    .catch(err => console.log(err));
+};
 
-    return (
-            SpotAPIUtil.createSpot(spot)
-                .then(spotRes => dispatch(receiveSpot(spotRes)))
-                .catch(err => console.log(err))
+export const updateSpot = spot => dispatch =>
+  SpotAPIUtil.updateSpot(spot)
+    .then(spotRes => dispatch(receiveSpot(spotRes)))
+    .catch(err => console.log(err));
 
-
-    )
-}
-
-export const updateSpot = (spot) => dispatch => (
-    SpotAPIUtil.updateSpot(spot)
-        .then(spotRes => dispatch(receiveSpot(spotRes)))
-        .catch(err => console.log(err))
-)
-
-export const deleteSpot = (id) => dispatch => (
-    SpotAPIUtil.deleteSpot(id)
-        .then(res => dispatch(removeSpot(res.id)))
-        .catch(err => console.log(err))
-)
+export const deleteSpot = id => dispatch =>
+  SpotAPIUtil.deleteSpot(id)
+    .then(res => dispatch(removeSpot(res.id)))
+    .catch(err => console.log(err));
--- client/src/components/search_list/search_list_container.jsx
+++ client/src/components/search_list/search_list_container.jsx
@@ -1,39 +1,34 @@
-import { connect } from 'react-redux';
-import React from 'react';
-import { Link } from 'react-router-dom';
-import SearchList from './search_list'
-import { clearErrors } from '../../actions/errors_actions';
-import { fetchSpotsByGPS, fetchSpotsByZip } from '../../actions/spot_actions';
-const msp = (state,ownProps) =>{
+import { connect } from "react-redux";
+import React from "react";
+import { Link } from "react-router-dom";
+import SearchList from "./search_list";
+import { clearErrors } from "../../actions/errors_actions";
+import { fetchSpotsByGPS, fetchSpotsByZip } from "../../actions/spot_actions";
+const msp = (state, ownProps) => {
+  let indexfirstload;
+  if (typeof state.entities.spots.indexfirstload == "undefined") {
+    //Page was loaded once already, so assume there is no reults matching
+    indexfirstload = false;
+  } else {
+    indexfirstload = state.entities.spots.indexfirstload;
+  }
+  return {
+    currentUser: state.isAuthenticated.user,
+    isAuthenticated: state.isAuthenticated.isAuthenticated,
+    spots: state.entities.spots,
+    zip: state.entities.spots.zip,
+    range: state.entities.spots.range,
+    indexfirstload: indexfirstload
+  };
+};
 
-let indexfirstload
-if(typeof state.entities.spots.indexfirstload == 'undefined'){
-  //Page was loaded once already, so assume there is no reults matching
-  indexfirstload = false
-}
-else{
-  indexfirstload = state.entities.spots.indexfirstload
-}
-return ({
-  currentUser: state.isAuthenticated.user,
-  isAuthenticated: state.isAuthenticated.isAuthenticated,
-  spots: state.entities.spots,
-  zip: state.entities.spots.zip,
-<<<<<<< HEAD
-  range: state.entities.spots.range,
-  indexfirstload : indexfirstload,
-})}
-=======
-  range: state.entities.spots.ranges
-})
->>>>>>> alfred
+const mdp = dispatch => ({
+  clearErrors: () => dispatch(clearErrors()),
+  fetchSpotsByGPS: gps => dispatch(fetchSpotsByGPS(gps)),
+  fetchSpotsByZip: zip => dispatch(fetchSpotsByZip(zip))
+});
 
-
-const mdp = (dispatch) =>({
-clearErrors: () => dispatch(clearErrors()),
-fetchSpotsByGPS: (gps) => dispatch(fetchSpotsByGPS(gps)),
-fetchSpotsByZip: (zip) => dispatch(fetchSpotsByZip(zip))
-})
-
-
-export default connect(msp,mdp)(SearchList)
+export default connect(
+  msp,
+  mdp
+)(SearchList);
