510467352d380084af72148dd62b6b3271dd245f
Nicholas Halloran <nmhalloran@gmail.com>
2018-07-02 10:15:03 -0700

    heroku formatting

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,10 @@ module.exports = {
           query: {
             presets: ["env", "react"]
           }
-        }
+        },
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"]
       }
     ]
   },
