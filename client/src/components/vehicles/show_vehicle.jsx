a6f98a8a4ba85251fe35cebfb016c799aaf5ddb9
Meenakshi-Anand <abhi@codeanand.com>
2018-07-01 19:53:01 -0700

    add styling for show vehicles

--- client/src/components/vehicles/show_vehicle.jsx
+++ client/src/components/vehicles/show_vehicle.jsx
@@ -41,15 +41,24 @@ class ShowVehicle extends React.Component{
     }else{
     return (
       <div className="showVehicle">
-        <h1>Vehicle Show Page</h1>
-        <h2>Vehicle Type: {this.props.vehicle.vehicle_type}</h2>
-        <h2>Plate No: {this.props.vehicle.plate_no}</h2>
-        <h2>Color: {this.props.vehicle.color}</h2>
-        <h2>Model: {this.props.vehicle.model}</h2>
-        <h2>Year: {this.props.vehicle.year}</h2>
-        <button value="Delete Vehicle" onClick={ (e) => this.handleDelete(e) }>
-          Delete Vehicle
-        </button>
+        <div className="showVehicle-Info">
+          <div className="vehicle-Info">
+            <h3>Vehicle Information</h3>
+            <div className="vehicle-info-body">
+              <h5>Vehicle Type: {this.props.vehicle.vehicle_type}</h5>
+              <h5>Plate No: {this.props.vehicle.plate_no}</h5>
+              <h5>Color: {this.props.vehicle.color}</h5>
+              <h5>Model: {this.props.vehicle.model}</h5>
+              <h5>Year: {this.props.vehicle.year}</h5>
+           </div>
+           <div className="vehicle-info-button">
+              <button value="Delete Vehicle"
+                onClick={ (e) => this.handleDelete(e) }>
+                Delete Vehicle
+              </button>
+          </div>
+        </div>
+        </div>
       </div>
     );
   }
