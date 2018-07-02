63d24f0879e84766eab71edab7c4abcf11dbbae0
Nicholas Halloran <nmhalloran@gmail.com>
2018-07-02 10:23:22 -0700

    heroku

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Root from "./root.jsx";
 import configureStore from "./store";
-// import registerServiceWorker from './registerServiceWorker';
 
 const store = configureStore();
 
 ReactDOM.render(<Root store={store} />, document.getElementById("root"));
-
-// registerServiceWorker();
