e86eec117e654abfc8bf12e8382548e34de1cf96
AdoboFrenzy <alejandrino.alfred@gmail.com>
2018-07-02 20:09:51 -0700

    heroku

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   context: __dirname,
-  entry: "./src/index.js",
+  entry: ['babel-polyfill', __dirname + "/src/index.js"],
   output: {
     path: path.resolve(__dirname),
     filename: "bundle.js"
