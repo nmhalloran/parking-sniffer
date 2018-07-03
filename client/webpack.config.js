0267db0c5cac9e98578a74b6fcf843c09b4e371f
clustermass <clustermass2@gmail.com>
2018-07-02 18:21:05 -0700

    heroku7

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   context: __dirname,
-  entry: "./client/src/index.js",
+  entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname),
     filename: "bundle.js"
